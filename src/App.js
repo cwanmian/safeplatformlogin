9a4df97db4610c83c496d8f8245d9d24b95074db
ccwme <cwanmian@gmail.com>
2022-09-22 09:54:32 +0800

    20220922

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
 import './App.css';
 import 'antd/dist/antd.min.css'
 import Login from "./Components/Login";
+import {useState, useEffect} from "react";
+import {Button} from "antd";
+import Register from "./Components/Register";
+
 function App() {
-  return (
-    <div className="App">
-        <div className="content">
-            <Login></Login>
+    const [showLogin, setshowLogin] = useState(true)
+    return (
+        <div className="App">
+            <div className="bg">
+                <img src="./bg2.jpg" alt="加载中。。。"/>
+            </div>
+            <div className="content">
+                <div className="Login-header">
+                    {showLogin ? <Button onClick={() => {
+                        setshowLogin(false)
+                    }}>注册</Button> : <Button onClick={() => {
+                        setshowLogin(true)
+                    }}>返回</Button>}
+                </div>
+                <div className="loginContent">
+                    <div className="indexLogo">
+                        <a>BUGManager</a>
+                    </div>
+                    <Login show={showLogin}></Login>
+                    <Register show={!showLogin} setShowLogin={setshowLogin}></Register>
+                </div>
+                <div className="noneDiv"></div>
+            </div>
         </div>
-    </div>
-  );
+    );
 }
 
 export default App;
--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,9 +1,26 @@
-import { Button, Checkbox, Form, Input } from 'antd';
+import {Button, Checkbox, Form, Input, Spin} from 'antd';
 import React from 'react';
-
-const App = () => {
+import {CSSTransition} from 'react-transition-group'
+import "./Login.css"
+import axios from "axios";
+import {useState} from "react";
+const App = (props) => {
+    // axios.defaults.withCredentials=true
+    const [showLoading,setshowLoading]=useState(false)
     const onFinish = (values) => {
         console.log('Success:', values);
+        const{username,password}=values
+        setshowLoading(true)
+        axios.post("./login", {uname: username, pw: password}).then((res) => {
+            setshowLoading(false)
+            if(res.data.code===200){
+                window.location.href="/indexPage"
+            }
+            console.log(res)
+        }, (res) => {
+            setshowLoading(false)
+            console.log(res)
+        })
     };
 
     const onFinishFailed = (errorInfo) => {
@@ -11,69 +28,83 @@ const App = () => {
     };
 
     return (
-        <Form
-            name="basic"
-            labelCol={{
-                span: 8,
-            }}
-            wrapperCol={{
-                span: 16,
-            }}
-            initialValues={{
-                remember: true,
-            }}
-            onFinish={onFinish}
-            onFinishFailed={onFinishFailed}
-            autoComplete="off"
-        >
-            <Form.Item
-                label="用户名"
-                name="username"
-                rules={[
-                    {
-                        required: true,
-                        message: 'Please input your username!',
-                    },
-                ]}
-            >
-                <Input />
-            </Form.Item>
+        <div className="showLogin">
 
-            <Form.Item
-                label="密码"
-                name="password"
-                rules={[
-                    {
-                        required: true,
-                        message: 'Please input your password!',
-                    },
-                ]}
+            <CSSTransition
+                in={props.show}
+                timeout={300}
+                classNames="showLogin"
+                unmountOnExit
+                onExited={props.setRegistershow}
             >
-                <Input.Password />
-            </Form.Item>
+                <Form
+                    className="showLoginForm"
+                    name="basic"
+                    labelCol={{
+                        span: 8,
+                    }}
+                    wrapperCol={{
+                        span: 16,
+                    }}
+                    initialValues={{
+                        remember: true,
+                    }}
+                    onFinish={onFinish}
+                    onFinishFailed={onFinishFailed}
+                    autoComplete="off"
+                >
+                    <Form.Item
+                        label="用户名"
+                        name="username"
+                        rules={[
+                            {
+                                required: true,
+                                message: '请输入用户名!',
+                            },
+                        ]}
+                    >
+                        <Input/>
+                    </Form.Item>
 
-            <Form.Item
-                name="remember"
-                valuePropName="checked"
-                wrapperCol={{
-                    offset: 8,
-                    span: 16,
-                }}
-            >
-                <Checkbox>记住密码</Checkbox>
-            </Form.Item>
+                    <Form.Item
+                        label="密码"
+                        name="password"
+                        rules={[
+                            {
+                                required: true,
+                                message: '请输入密码!',
+                            },
+                        ]}
+                    >
+                        <Input.Password/>
+                    </Form.Item>
 
-            <Form.Item
-                wrapperCol={{
-                    offset: 8,
-                    span: 16,
-                }}
-            >
-                <Button type="primary" htmlType="submit">
-                    Submit
-                </Button>
-            </Form.Item>
-        </Form>
+                    <Form.Item
+                        name="remember"
+                        valuePropName="checked"
+                        wrapperCol={{
+                            offset: 8,
+                            span: 16,
+                        }}
+                    >
+                        <Checkbox>记住密码</Checkbox>
+                    </Form.Item>
+
+                    <Form.Item
+                        wrapperCol={{
+                            offset: 8,
+                            span: 16,
+                        }}
+                    >
+                        <Button type="primary" htmlType="submit" loading={showLoading}>
+                            登录
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </CSSTransition>
+
+        </div>
+
     );
 };
 
